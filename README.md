a68088e4436b10f6f094faa5de833967dcd67b4f
Guilherme <grsguiga@gmail.com>
2023-06-08 02:06:12 -0300

    feat: create auto-lessons.js

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# project-auto-lessons
+# AutoLessons
+
+O AutoLessons é um script desenvolvido para automatizar a conclusão de lições no curso da Trybe. Com este projeto, você pode economizar tempo e esforço, permitindo que o script interaja automaticamente com as aulas, concluindo tarefas e avançando para a próxima lição.
+
+## Funcionalidades
+
+- Clica automaticamente nos links do dia das aulas.
+- Clica automaticamente nos links de conteúdo e botões de conclusão.
+- Espera o tempo necessário para a renderização da página.
+
+## Como usar
+
+1. Abra a plataforma da Trybe no seu navegador.
+2. Acesse a página inicial do curso.
+3. Acesse o módulo desejado.
+4. Acesse qualquer dia da seção desejada(o AutoLessons vai verificar todos os dias de qualquer forma).
+5. Abra o console do navegador (geralmente pressionando F12).
+6. Cole o código do script fornecido no console(arquivo auto-lessons neste repositório).
+7. Aguarde o script navegar automaticamente pelas aulas e concluir as lições.
+8. Acompanhe o progresso no console do navegador.
+
+## Requisitos
+
+- Um navegador da web (Google Chrome, Mozilla Firefox, etc.).
+- Acesso ao curso da Trybe.
+
+## Observações
+
+- Este projeto foi desenvolvido apenas para fins educacionais e de automação. Utilize com responsabilidade e ética.
+- O desempenho do script pode variar dependendo da plataforma e banda larga.
+
+## Contribuição
+
+Contribuições são bem-vindas! Se você tiver ideias para melhorar este projeto, sinta-se à vontade para abrir uma issue ou enviar um pull request.
+
+## Licença
+
+Este projeto é licenciado sob a [MIT License](LICENSE).
---
+++ auto-lessons.js
@@ -0,0 +1,64 @@
+// Função para clicar no elemento
+function clickElement(element) {
+  element.click();
+}
+
+// Obter os elementos que representam os dias de aula
+const diasAulas = document.getElementsByClassName('c-jpoxul icon');
+const quantidadeDiasAulas = diasAulas.length;
+
+// Função para iterar sobre os dias de aula e clicar nos conteúdos
+function processDiaAula(diaIndex) {
+  if (diaIndex >= quantidadeDiasAulas) {
+    console.log('Processo concluído!');
+    return;
+  }
+
+  const diaAula = diasAulas[diaIndex];
+  console.log('Clicando no dia da aula', diaIndex + 1);
+
+  // Obter os conteúdos do dia atual
+  const conteudosDia = diaAula.nextElementSibling.getElementsByClassName('c-hKhFYS');
+  const quantidadeConteudosDia = conteudosDia.length;
+
+  // Função para clicar no botão de conclusão do conteúdo
+  function processConteudo(conteudoIndex) {
+    if (conteudoIndex >= quantidadeConteudosDia) {
+      // Avançar para o próximo dia de aula
+      diaIndex++;
+        processDiaAula(diaIndex);
+      return;
+    }
+
+    const conteudo = conteudosDia[conteudoIndex];
+
+    // Clicar no link do conteúdo
+    if (conteudo) {
+      console.log('Clicando no conteúdo', conteudoIndex + 1, 'do dia', diaIndex + 1);
+      clickElement(conteudo);
+    }
+
+    // Aguardar 5 segundos
+    setTimeout(() => {
+      // Clicar no botão de conclusão do conteúdo
+      const button = document.getElementsByClassName("c-eqzuyj c-eqzuyj-dbhHqa-size-medium c-eqzuyj-bEHIds-kind-primary conclude-button__ada-button")[0];
+
+      if (button) {
+        console.log('Clicando em lição concluída!')
+        clickElement(button);
+      } else {
+        console.log('Lição já foi concluída!');
+      }
+
+      // Avançar para o próximo conteúdo
+      conteudoIndex++;
+      processConteudo(conteudoIndex);
+    }, 5000);
+  }
+
+  // Iniciar o processo para o primeiro conteúdo do dia
+  processConteudo(0);
+}
+
+// Chamar a função para iniciar o processo no primeiro dia de aula
+processDiaAula(0);
